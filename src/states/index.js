0870e8268b7aa74aa592b618dcf69ec94bf629c5
Ali Nurdin <alinurdin159@gmail.com>
2023-12-19 07:44:26 +0700

    init commit

---
+++ src/states/index.js
@@ -0,0 +1,23 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authUserReducer from './authUser/reducer';
+import isPreloadReducer from './isPreload/reducer';
+import usersReducer from './users/reducer';
+import talksReducer from './talks/reducer';
+import talkDetailReducer from './talkDetail/reducer';
+import { loadingBarReducer } from 'react-redux-loading-bar';
+
+/**
+ * @TODO: Create Redux store
+ */
+const store = configureStore({
+  reducer: {
+    authUser: authUserReducer,
+    isPreload: isPreloadReducer,
+    users: usersReducer,
+    talks: talksReducer,
+    talkDetail: talkDetailReducer,
+    loadingBar: loadingBarReducer,
+  },
+});
+
+export default store;
---
+++ src/states/isPreload/action.js
@@ -0,0 +1,43 @@
+/**
+ * @TODO: Define all the actions (creator) for the isPreLoad state
+*/
+
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreLoad) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreLoad,
+    },
+  };
+}
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    dispatch(showLoading());
+
+    try {
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      dispatch(setIsPreloadActionCreator(false));
+    }
+
+    dispatch(hideLoading());
+  };
+}
+
+export {
+  ActionType,
+  setIsPreloadActionCreator,
+  asyncPreloadProcess,
+};
